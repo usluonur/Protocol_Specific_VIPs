d403890256b7b1f7df73e5d4dc55ca78c5a46446
Onur Uslu <usl.onur@gmail.com>
2021-02-14 12:17:30 +0300

    Folders are changed

---
+++ SystemVerilog_codes/VIP Template/template_agent.svh
@@ -0,0 +1,9 @@
+class template_agent extends uvm_component;
+   // UVM Factory Registration
+   //
+   `uvm_component_utils(template_agent)
+
+   //--------------------------------------
+   //Data Members
+   //--------------------------------------
+endclass
