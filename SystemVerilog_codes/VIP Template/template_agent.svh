553541cfad8c1fa798e52a96f3692994599f1664
Onur Uslu <usl.onur@gmail.com>
2021-02-14 17:37:17 +0300

    Unnecesary file removed

--- SystemVerilog_codes/VIP Template/template_agent.svh
+++
@@ -1,9 +0,0 @@
-class template_agent extends uvm_component;
-   // UVM Factory Registration
-   //
-   `uvm_component_utils(template_agent)
-
-   //--------------------------------------
-   //Data Members
-   //--------------------------------------
-endclass
